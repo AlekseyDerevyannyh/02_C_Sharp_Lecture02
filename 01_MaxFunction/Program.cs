4c7f391b31cde1444a3735aeb06e23beea6c03ef
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-11 16:58:47 +0300

    add Example 1 Search max of 9 with Function and Array

--- 01_MaxFunction/Program.cs
+++ 01_MaxFunction/Program.cs
@@ -1,5 +1,18 @@
-﻿// Программа поиска максимального элемента 
+﻿// Программа поиска максимального элемента из 9 с использованием функций
 using System;
 using static System.Console;
 
+int[] number = {15, 21, 39, 12, 23, 57, 13, 23, 33};
+Clear();
+int Max (int arg1, int arg2, int arg3) {
+	int result = arg1;
+	if (arg2 > result)	result = arg2;
+	if (arg3 > result)	result = arg3;
+	return result;
+}
 
+int max = Max(
+	Max(number[0], number[1], number[2]),
+	Max(number[3], number[4], number[5]),
+	Max(number[6], number[7], number[8]));
+Write($"Максимальное число = {max}");
